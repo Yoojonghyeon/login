b0abc242a146f087b83940b6733bc056bdf92b90
YooJongHyeon <justin77748292@gmail.com>
2022-03-19 21:57:51 +0900

    commit

--- login/lecture/login.js
+++ login/lecture/login.js
@@ -6,23 +6,33 @@ const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.dir(event);
+  //console.dir(event);
 
   const target = event.currentTarget;
   const email = target[0].value;
   const password = target[1].value;
   const err = document.querySelector(".err");
 
-  // node.js에서 GET 요청으로 원격 이미지 가져오기
-  axios
-    .get("http://localhost:8882/login", {
-      email: email,
-      password: password,
+  axios({
+    method: "get",
+    url: "http://localhost:8882/login",
+  })
+    .then((res) => {
+      console.log(res.data);
     })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error.response);
+    .catch((err) => {
+      console.log(err.response);
     });
+
+  // axios
+  //   .get("http://localhost:8882/login", {
+  //     email: email,
+  //     password: password,
+  //   })
+  //   .then((response) => {
+  //     console.log(response);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error.response.data.message);
+  //   });
 });
