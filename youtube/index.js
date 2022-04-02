4c6f8e6d7d4f5fa42635c710e3553e2c1c63a90a
YooJongHyeon <justin77748292@gmail.com>
2022-04-02 16:22:19 +0900

    youtube api

--- youtube/index.js
+++ youtube/index.js
@@ -1,38 +1,43 @@
 /* Youtube API */
 const YOUTUBE_API = "AIzaSyDU2mRG2DV9eKUACzHUJ6w55LrhGurVeXk";
+/* ------------------------------ */
+
 // 페이지 구성
 //       - 메인페이지(상단 검색창 + 영상리스트) t
 //       - 검색창 마크업 t
 //       - 검색창에 단어 입력한거 핸들링 t
-//       - 검색한 단어를 API로 비동기 호출 f
+//       - 검색한 단어를 API로 비동기 호출 t
 //       - 검색 결과 확인 (리스트[배열]) → 썸네일이미지 → 리스트 하나의 요소가 어떤 형식인지 파악하는것이 중요 f
 //       - 썸네일 이미지 + 제목 을 하나의 아이템으로 만들어 리스트로 만들어주기 f
 
 const search = document.querySelector(".search");
-//const searchWord = document.querySelector(".search__word").value;
+const thumbnails = document.querySelector(".video__thumbnails");
+const title = document.querySelector(".video__title");
 
 search.addEventListener("submit", (event) => {
   event.preventDefault();
   const target = event.currentTarget;
   const searchWord = target[0].value; //검색어 값
+  const data = {
+    key: YOUTUBE_API, //발급 키 string
+    part: "snippet", // 관련 영상
+    q: searchWord, // 검색어
+    maxResults: 10, //영상 최대 개수
+    type: "video",
+    videoDuration: "long", //시간의 길이 hmm
+  };
+  const video = []; //검색한 영상 담을 그릇
 
-  const video = [];
-
-  axios.defaults.baseURL = "https://www.googleapis.com/youtube/v3";
-
+  //       - 검색한 단어를 API로 비동기 호출
   axios
-    .get("/search", {
-      key: YOUTUBE_API, //발급 키 string
-      part: "snippet", // 관련 영상
-      q: searchWord, // 검색어
-      maxResults: 10, //영상 최대 개수
-      type: "video",
-      videoDuration: "long", //시간의 길이 hmm
-    })
+    .get("https://www.googleapis.com/youtube/v3/search", { data })
     .then((res) => {
-      console.log(res.data);
+      //console.log(res.data.items);
+      const item = res.data.items;
+      video.push(item);
+      console.log(video);
     })
     .catch((err) => {
-      console.log(err.response);
+      console.log(err.response.data.error.message);
     });
 });
