ccd25d6febf4646815433487da1bf3cfa63a7ef2
YooJongHyeon <justin77748292@gmail.com>
2022-04-13 03:28:48 +0900

    commit

--- youtube/index.js
+++ youtube/index.js
@@ -1,14 +1,14 @@
 /* Search URL */
 const SEARCH_URL = "https://www.googleapis.com/youtube/v3/search?";
 /* Youtube API */
-const YOUTUBE_API = "AIzaSyDp1xf-c4d1doTJq7xp0pVpG9J7gcE1-wc";
+//const YOUTUBE_API = "AIzaSyDp1xf-c4d1doTJq7xp0pVpG9J7gcE1-wc";
 /* ------------------------------ */
 
 // 페이지 구성
 //       - 메인페이지(상단 검색창 + 영상리스트) t
 //       - 검색창 마크업 t
 //       - 검색창에 단어 입력한거 핸들링 t
-//       - 검색한 단어를 API로 비동기 호출 t
+//       - 검색한 단어를 API로 비동기 호출 tzzz
 //       - 검색 결과 확인 (리스트[배열]) → 썸네일이미지 → 리스트 하나의 요소가 어떤 형식인지 파악하는것이 중요 f
 //       - 썸네일 이미지 + 제목 을 하나의 아이템으로 만들어 리스트로 만들어주기 f
 
@@ -32,7 +32,7 @@ search.addEventListener("submit", (event) => {
 
   //       - 검색한 단어를 API로 비동기 호출
   axios
-    .get(SEARCH_URL, { data })
+    .get("https://www.googleapis.com/youtube/v3/search?", { data })
     .then((res) => {
       console.log(res.data.items);
       const item = res.data.items;
